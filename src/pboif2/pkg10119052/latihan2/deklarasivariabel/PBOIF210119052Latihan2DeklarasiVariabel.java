66b8669267014df235a0554282c2ab6a4f277239
Kiki <Kiki@Kiki-PC>
2020-10-17 22:03:59 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan2/deklarasivariabel/PBOIF210119052Latihan2DeklarasiVariabel.java
@@ -0,0 +1,35 @@
+package pboif2.pkg10119052.latihan2.deklarasivariabel;
+
+/**
+ *
+ * Nama : Rizki Dwi Nugraha
+ * Kelas : IF-2
+ * NIM : 10119052
+ * 
+ */
+public class PBOIF210119052Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta Uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
